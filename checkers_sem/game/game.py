0a857fac05d993e29d45998431df87a9c23d9c2a
beczatom <beczatom@fit.cvut.cz>
2025-04-13 16:36:29 +0200

    Move making

--- checkers_sem/game/game.py
+++ checkers_sem/game/game.py
@@ -9,3 +9,29 @@ class Game:
         self.board = Board()
         self.moves_stack = deque[Move, int]()
         self.last_take = 0
+
+    def push(self, move : Move):
+        self.last_take += 1
+
+        if move.move_type & TAKE:
+            self.last_take = 0
+
+        self.moves_stack.append((move, self.last_take))
+
+        self.board.make_move(move)
+
+    def pop(self):
+        last_move, _ = self.moves_stack[-1]
+
+        self.board.undo_move(last_move)
+
+        self.moves_stack.pop()
+
+        if len(self.moves_stack) != 0:
+            self.last_take = self.moves_stack[-1][1]
+        else:
+            self.last_take = 0
+
+    def peek(self):
+        return self.moves_stack[-1][0]
+
