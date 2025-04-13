44f2d7c29023ea341bca2c66c40d3e6858f05439
beczatom <beczatom@fit.cvut.cz>
2025-04-13 16:37:03 +0200

    Get result of game

--- checkers_sem/game/game.py
+++ checkers_sem/game/game.py
@@ -2,6 +2,12 @@ from checkers_sem.game.board import Board
 from checkers_sem.game.move import *
 from collections import deque
 
+def tree_fold_repetition(moves_stack : deque[Move, int]) -> bool:
+    if len(moves_stack) < 6:
+        return False
+
+    return moves_stack[-1][0] == moves_stack[-3][0].revert() == moves_stack[-5][0] and \
+        moves_stack[-2][0] == moves_stack[-4][0].revert() == moves_stack[-6][0]
 
 class Game:
 
@@ -34,4 +40,20 @@ class Game:
 
     def peek(self):
         return self.moves_stack[-1][0]
+
+    def get_moves(self):
+        if self.last_take < 25:
+            return self.board.get_legal_moves()
+        return []
+
+    def get_result(self) -> tuple | None:
+        if self.board.black.bit_count() == 0:
+            return 1, 0
+
+        if self.board.white.bit_count() == 0:
+            return 0, 1
+
+        if self.last_take > 50 or tree_fold_repetition(self.moves_stack):
+            return 1 / 2, 1 / 2
+        return None
 
