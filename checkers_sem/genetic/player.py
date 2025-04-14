ac5a7eb9d7bec331c75a4450298a89f3d3395cee
beczatom <beczatom@fit.cvut.cz>
2025-04-14 22:28:17 +0200

    Player position evaluate function

--- checkers_sem/genetic/player.py
+++ checkers_sem/genetic/player.py
@@ -3,3 +3,14 @@ from checkers_sem.game.game import *
 class Player:
     def __init__(self, coefs : np.array):
         self.coefs = np.array(coefs, dtype=np.float64)
+
+    def evaluate(self, game : Game) -> float:
+        possible_res = game.get_result()
+
+        if possible_res is not None:
+            if possible_res[0] == 1: return np.inf
+            if possible_res[1] == 1: return -np.inf
+            return 0
+
+        return np.dot(self.coefs, game.board.stats())
+
