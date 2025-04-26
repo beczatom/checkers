c63f2be928e54045d8490df967e3dbb86cd08352
beczatom <beczatom@fit.cvut.cz>
2025-04-26 21:07:49 +0200

    Human vs human / pc modes added

--- checkers_sem/gui/main.py
+++ checkers_sem/gui/main.py
@@ -1,9 +1,6 @@
-import pygame
-
-from checkers_sem.constants import *
 from checkers_sem.gui.menu import *
 from checkers_sem.gui.game_window import *
-from checkers_sem.genetic.player import Player
+from checkers_sem.player.player import AIPlayer, HumanPlayer
 
 class Main:
     def __init__(self):
@@ -16,12 +13,12 @@ class Main:
 
         match option:
             case GameType.HUMAN_VS_HUMAN:
-                pass
+                game = GameWindow(self.screen, (HumanPlayer(), HumanPlayer()))
             case GameType.HUMAN_VS_PC:
-                pass
+                game = GameWindow(self.screen, (HumanPlayer(), AIPlayer()))
+
             case GameType.PC_VS_PC:
-                game = GameWindow(self.screen, (Player([3.6, 10.6, 4.4, 2.7, 5.2, 3.2]),
-                                                       Player([5.5, 6, 4, 5.4, 6.4, 3.2])) )
+                game = GameWindow(self.screen, (AIPlayer(), AIPlayer()))
             case _:
                 pygame.quit()
                 return
