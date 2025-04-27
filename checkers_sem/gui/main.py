9832d2eaa44eec215f6358361a62a801a5b87ccf
beczatom <beczatom@fit.cvut.cz>
2025-04-27 19:34:31 +0200

    Main cleaner

--- checkers_sem/gui/main.py
+++ checkers_sem/gui/main.py
@@ -1,6 +1,5 @@
 from checkers_sem.gui.menu import *
 from checkers_sem.gui.game_window import *
-from checkers_sem.player.player import AIPlayer, HumanPlayer
 
 class Main:
     def __init__(self):
@@ -9,21 +8,7 @@ class Main:
         pygame.display.set_caption(MENU_TITLE)
 
     def show(self):
-        option = Menu(self.screen).show()
-
-        match option:
-            case GameType.HUMAN_VS_HUMAN:
-                game = GameWindow(self.screen, (HumanPlayer(), HumanPlayer()))
-            case GameType.HUMAN_VS_PC:
-                game = GameWindow(self.screen, (HumanPlayer(), AIPlayer()))
-
-            case GameType.PC_VS_PC:
-                game = GameWindow(self.screen, (AIPlayer(), AIPlayer()))
-            case _:
-                pygame.quit()
-                return
-
-        game.play()
+        Menu(self.screen).show()
         pygame.quit()
 
 
