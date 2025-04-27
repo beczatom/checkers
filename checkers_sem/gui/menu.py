d704557468d614e7c3d39dbcc767f0d4f358153d
beczatom <beczatom@fit.cvut.cz>
2025-04-27 19:34:58 +0200

    Menu cleaner

--- checkers_sem/gui/menu.py
+++ checkers_sem/gui/menu.py
@@ -1,5 +1,8 @@
 from checkers_sem.constants import *
 from checkers_sem.gui.utils.button import Button
+from checkers_sem.gui.game_window import GameWindow
+from checkers_sem.gui.genetic_setting_window import GeneticSettingWindow
+from checkers_sem.player.player import *
 import pygame
 
 
@@ -9,50 +12,60 @@ class Menu:
         self.screen.fill(BACKGROUND_COLOR)
         self.buttons = self.buttons_init()
 
+        self.next_window = None
+
     def buttons_init(self) -> list[Button]:
+
+        def human_vs_human():
+            self.next_window = GameWindow(self.screen, (HumanPlayer(), HumanPlayer()))
+
+        def human_vs_pc():
+            self.next_window = GameWindow(self.screen, (HumanPlayer(), AIPlayer()))
+
+        def pc_vs_pc():
+            self.next_window = GameWindow(self.screen, (AIPlayer(), AIPlayer()))
+
+        def genetic():
+            self.next_window = GeneticSettingWindow(self.screen)
+
+        actions = [human_vs_human, human_vs_pc, pc_vs_pc, genetic]
+
         buttons = []
 
-        margin_x = SCREEN_WIDTH // 8
-        margin_y_up = 3 * SCREEN_HEIGHT // 5
-        padding_y = SCREEN_HEIGHT // 16
+        margin_x = self.screen.get_width() // 8
+        margin_y_up =  self.screen.get_height() // 2
+        padding_y = self.screen.get_height() // 24
 
-        button_width = (SCREEN_WIDTH - 2 * margin_x)
-        button_height = (SCREEN_HEIGHT - margin_y_up - 2 * padding_y) // 3
+        button_width = (self.screen.get_width() - 2 * margin_x)
+        button_height = (self.screen.get_height() - margin_y_up - 3 * padding_y) // 4
 
-        for i, text in enumerate([HUMAN_VS_HUMAN_TEXT, HUMAN_VS_PC_TEXT, PC_VS_PC_TEXT]):
-            rect_center_x = SCREEN_WIDTH // 2
-            rect_center_y = margin_y_up + i * button_height + i * padding_y
+        for i, text in enumerate([HUMAN_VS_HUMAN_TEXT, HUMAN_VS_PC_TEXT, PC_VS_PC_TEXT, GENETIC_TEXT]):
+            top = margin_y_up + i * button_height + i * padding_y
+            left = margin_x
 
-            rect = pygame.Rect(rect_center_x - button_width // 2, rect_center_y - button_height // 2, button_width,
-                               button_height)
+            rect = pygame.Rect(left, top, button_width,button_height)
 
-            buttons.append(Button(self.screen, rect, text, background_color=BACKGROUND_COLOR,
-                                  first_border=True, second_border=True, font_size= 25))
-            buttons[-1].draw(pygame.mouse.get_pos())
+            buttons.append(Button(self.screen.subsurface(rect), (left, top), text, actions[i],
+                                  background_color=BACKGROUND_COLOR, first_border=True, second_border=True, font_size= 25))
+
+            buttons[-1].draw()
 
         return buttons
 
-    def show(self) -> GameType:
-        run = True
-        option = 0
-
-        while run and option == 0:
+    def show(self):
+        while self.next_window is None:
             pygame.time.delay(10)
-            mouse_pos = pygame.mouse.get_pos()
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
 
-                if event.type == pygame.MOUSEMOTION:
-                    for button in self.buttons:
-                        button.draw(mouse_pos)
-
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    for i, button in enumerate(self.buttons):
-                        if button.clicked(mouse_pos):
-                            option = i + 1
+                for button in self.buttons:
+                    # try:
+                        button.handle_event(event)
+                    # except Exception as e:
+                    #     print('returned exception', e.args)
+                    #     return
 
             pygame.display.update()
 
-        return option
+        self.next_window.show()
+
