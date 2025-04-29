95c2e4bee33c2002bbc16c8abaf1f364da8f6064
beczatom <beczatom@fit.cvut.cz>
2025-04-29 22:39:18 +0200

    Tile and button with onclick function for cleaner code

--- checkers_sem/gui/utils/button.py
+++ checkers_sem/gui/utils/button.py
@@ -29,26 +29,29 @@ class Button(Widget):
         self.background_color = BACKGROUND_COLOR
         self.text_color = TEXT_COLOR
         self.onclick = onclick
+        self.is_hovered = False
 
     def hover(self):
         self.background_color = HOVER_BACKGROUND_COLOR
         self.text_color = HOVER_TEXT_COLOR
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
 
     def unhover(self):
         self.background_color = BACKGROUND_COLOR
         self.text_color = TEXT_COLOR
+        self.is_hovered = False
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
     def handle_event(self, event : pygame.event.Event):
-        if event.type == pygame.QUIT:
-            raise Exception('End')
-
         if event.type == pygame.MOUSEMOTION:
             if self.screen_rect.collidepoint(event.pos):
                 self.hover()
                 self.draw()
+                self.is_hovered = True
             else:
-                self.unhover()
-                self.draw()
+                if self.is_hovered:
+                    self.unhover()
+                    self.draw()
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if self.screen_rect.collidepoint(event.pos):
@@ -62,16 +65,35 @@ class Button(Widget):
         self.surface.blit(text, text_rect)
 
 class ImageButton(Widget):
-    def __init__(self, surface : pygame.Surface, left_top : tuple[int, int], background_image):
+    def __init__(self, surface : pygame.Surface, left_top : tuple[int, int], background_image : str, onclick : Callable[[], None] = None):
         super().__init__(surface, left_top)
         self.background_image = background_image
         self.background_image = pygame.image.load(self.background_image).convert_alpha()
         self.background_image = pygame.transform.scale(self.background_image, self.surface.get_rect().size)
+        self.onclick = onclick
+        self.is_hovered = False
 
 
-    def draw(self, mouse_pos : tuple[int, int] = (0, 0)):
+    def draw(self):
         self.surface.fill(BACKGROUND_COLOR)
         self.surface.blit(self.background_image, self.surface.get_rect().topleft)
 
-    def clicked(self, mouse_pos : tuple[int, int]) -> bool:
-        return self.screen_rect.collidepoint(mouse_pos)
+    def handle_event(self, event : pygame.event.Event):
+        if event.type == pygame.MOUSEMOTION:
+            if self.screen_rect.collidepoint(event.pos):
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                self.is_hovered = True
+            else:
+                if self.is_hovered:
+                    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                    self.is_hovered = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 \
+                and self.screen_rect.collidepoint(event.pos):
+            self.onclick()
+
+    # def clicked(self, mouse_pos : tuple[int, int]) -> bool:
+    #     if self.screen_rect.collidepoint(mouse_pos) and self.onclick:
+    #         self.onclick()
+    #         return True
+    #     return self.screen_rect.collidepoint(mouse_pos)
--- checkers_sem/gui/utils/tile.py
+++ checkers_sem/gui/utils/tile.py
@@ -2,66 +2,46 @@ import pygame
 from checkers_sem.gui.utils.button import ImageButton
 from checkers_sem.constants import *
 from checkers_sem.gui.utils.widget import Widget
-
+from collections.abc import Callable
 
 class Tile(Widget):
-    def __init__(self, surface : pygame.Surface, left_top : tuple[int, int], pos_mask : BitBoard, color_bool : TileColor):
+    def __init__(self, surface : pygame.Surface, left_top : tuple[int, int], pos_mask : BitBoard, onclick : Callable[[],None]):
         super().__init__(surface, left_top)
-        self.background = TILE_BACKGROUND[color_bool]
-        self.color_bool = color_bool
+        self.background = TILE_BACKGROUND
         self.pos_mask = pos_mask
         self.piece_img = None
         self.possible_move = False
-
-        if color_bool == TileColor.BLACK:
-            self.button = ImageButton(surface, self.left_top, TILE_BACKGROUND[color_bool])
+        self.button = ImageButton(surface, self.left_top, TILE_BACKGROUND, onclick)
 
 
     def put_piece_img(self, piece : Piece, piece_color : PieceColor):
-        if self.color_bool == TileColor.WHITE:
-            raise Exception('Invalid color')
-
         self.piece_img = PIECE_IMAGES[(piece, piece_color)]
         self.possible_move = False
 
     def put_possible_move(self):
         self.possible_move = True
 
-
     def clear_img(self):
-        if self.color_bool == TileColor.WHITE:
-            raise Exception('Invalid color')
-
         self.piece_img = None
         self.possible_move = False
 
-    def draw(self, mouse_pos : tuple[int, int] = (0, 0)):
-        if self.color_bool == TileColor.WHITE:
-            self.surface.fill(self.background)
-            # pygame.draw.rect(self.screen, self.background, self.rect)
-        else:
-            self.button.draw(mouse_pos)
-            if self.piece_img is not None:
-                surface_rect = pygame.Rect(self.surface.get_rect())
-                image_rect = pygame.Rect(surface_rect.x + surface_rect.width // 5, surface_rect.y + surface_rect.height // 5,
-                                         3 * surface_rect.width // 5, 3 * surface_rect.height // 5)
-                # image_rect = pygame.Rect(self.rect.x + self.rect.width // 5, self.rect.y + self.rect.height // 5,
-                #                          3 * self.rect.width // 5, 3 * self.rect.height // 5)
-                image = pygame.image.load(self.piece_img).convert_alpha()
-                image = pygame.transform.scale(image, image_rect.size)
-                self.surface.blit(image, image_rect)
+    def draw(self):
+        self.button.draw()
+        if self.piece_img is not None:
+            surface_rect = pygame.Rect(self.surface.get_rect())
+            image_rect = pygame.Rect(surface_rect.x + surface_rect.width // 5, surface_rect.y + surface_rect.height // 5,
+                                     3 * surface_rect.width // 5, 3 * surface_rect.height // 5)
+            image = pygame.image.load(self.piece_img).convert_alpha()
+            image = pygame.transform.scale(image, image_rect.size)
+            self.surface.blit(image, image_rect)
 
         if self.possible_move:
             image = pygame.image.load(POSSIBLE_MOVE_IMG).convert_alpha()
             image = pygame.transform.scale(image, self.surface.get_rect().size)
             self.surface.blit(image, self.surface.get_rect())
 
-    def clicked(self, mouse_pos) -> bool:
-        if self.color_bool == TileColor.WHITE:
-            raise Exception('Invalid color')
-        return self.button.clicked(mouse_pos)
+    def handle_event(self, event : pygame.event.Event):
+        return self.button.handle_event(event)
 
     def get_pos(self) -> BitBoard:
-        if self.color_bool == TileColor.WHITE:
-            raise Exception('Invalid color')
         return self.pos_mask
