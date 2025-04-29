3cd850c045202d9e0a3cc418e663294767225ed8
beczatom <beczatom@fit.cvut.cz>
2025-04-29 22:39:54 +0200

    Added function to determine whether the players time is going

--- checkers_sem/player/player.py
+++ checkers_sem/player/player.py
@@ -10,21 +10,24 @@ class Player:
         self.chessboard = chessboard
         self.time_left = 70
         self.last_start = None
+        self.time_going = False
 
     def set_chessboard(self, chessboard):
         self.chessboard = chessboard
 
     def time_start(self):
         self.last_start = time.time()
+        self.time_going = True
 
     def time_stop(self):
         self.time_left = self.get_time_left()
         self.last_start = None
+        self.time_going = False
 
     def get_time_left(self) -> float:
         return self.time_left - (time.time() - self.last_start) if self.last_start else self.time_left
 
-    def move(self, depth : int = MAX_TRAIN_DEPTH, clicked_mouse_pos : tuple[int, int] = None):
+    def move(self, depth : int = MAX_TRAIN_DEPTH):
         pass
 
 class AIPlayer(Player):
@@ -33,7 +36,7 @@ class AIPlayer(Player):
         self.player = GeneticPlayer(AI_COEFS)
 
     # performs move in game via AI genetic player
-    def move(self, depth : int = MAX_TRAIN_DEPTH, clicked_mouse_pos : tuple[int, int] = None):
+    def move(self, depth : int = MAX_TRAIN_DEPTH):
         no_moves = self.player.move(self.chessboard.game, depth)
         super().time_stop()
         return no_moves
@@ -45,29 +48,27 @@ class HumanPlayer(Player):
 
 
     # depth is always None, only for polymorphism compatibility
-    def move(self, depth : int = None, clicked_mouse_pos : tuple[int, int] = None):
-        if clicked_mouse_pos is None:
-            return
+    def move(self, depth : int = None):
 
         if self.last_clicked is not None:
-            clicked_now = self.chessboard.get_clicked_mask(clicked_mouse_pos)
+            clicked_now = self.chessboard.get_clicked_mask()
             if clicked_now in self.chessboard.game.get_moves_to_mask(self.last_clicked):
                 # move can be performed
                 self.chessboard.game.push(self.chessboard.game.get_move_from_to(self.last_clicked, clicked_now))
                 self.chessboard.reset_possible_moves()
                 self.last_clicked = None
                 super().time_stop()
-                return None
+                return False
 
         # so far nothing was clicked
-        self.last_clicked = self.chessboard.get_clicked_mask(clicked_mouse_pos)
+        self.last_clicked = self.chessboard.get_clicked_mask()
         if self.last_clicked not in self.chessboard.game.get_moves_from_mask():
             self.last_clicked = None
 
         self.chessboard.set_possible_moves(self.chessboard.game.get_moves_to_mask(self.last_clicked))
 
-        # returns 1. if the player lost by no more moves left, 2. if player made a move
+        # returns if the player lost by no more moves left
         if len(self.chessboard.game.get_moves_from_mask()) == 0:
-            return True, False
+            return True
 
-        return None
+        return False
