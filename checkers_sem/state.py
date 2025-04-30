032e6de95bd7e242ab866947e281aaea3a834e08
beczatom <beczatom@fit.cvut.cz>
2025-05-01 00:21:33 +0200

    Minor changes in state

--- checkers_sem/state.py
+++ checkers_sem/state.py
@@ -9,14 +9,10 @@ class State:
         self.CROSSOVER_PCT = CROSSOVER_PCT
         self.MUTATION_PCT = MUTATION_PCT
 
+        self.DEPTH = MAX_TRAIN_DEPTH
+
     def get_genetic_settings(self):
         return [self.POPULATION_SIZE, self.GENERATIONS, self.MAX_TRAIN_DEPTH, self.CROSSOVER_PCT, self.MUTATION_PCT]
-
-    # global POPULATION_SIZE = constants.POPULATION_SIZE
-    # global GENERATIONS = GENERATIONS
-    # global MAX_TRAIN_DEPTH = MAX_TRAIN_DEPTH
-    # global CROSSOVER_PCT = CROSSOVER_PCT
-    # global MUTATION_PCT = MUTATION_PCT
 
 
 state = State()
