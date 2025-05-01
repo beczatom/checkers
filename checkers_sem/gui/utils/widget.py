c9e42e5f022647ada8df95d3a77d22338ef093ba
beczatom <beczatom@fit.cvut.cz>
2025-05-01 23:34:10 +0200

    Added only one border for widgets

--- checkers_sem/gui/utils/widget.py
+++ checkers_sem/gui/utils/widget.py
@@ -2,15 +2,13 @@ import pygame
 
 from checkers_sem.constants import *
 
-def tuple_sum(tuple1 : tuple[int, int], tuple2 : tuple[int, int]):
-    return tuple1[0] + tuple2[0], tuple1[1] + tuple2[1]
-
 class Widget:
     def __init__(self, surface : pygame.Surface, screen_left_top : tuple[int, int]):
         self.screen_left_top = screen_left_top
         self.left_top = screen_left_top
         self.screen_rect = pygame.Rect(*screen_left_top, *surface.get_rect().size)
         self.surface = surface
+        self.surface.fill(BACKGROUND_COLOR)
         self.rect_without_border = surface.get_rect()
         self.background_color = BACKGROUND_COLOR
 
@@ -21,22 +19,20 @@ class Widget:
         return self.screen_rect.collidepoint(mouse_pos)
 
     def draw_border(self):
-        self.rect_without_border = self.__draw_borders()
+        self.__draw_borders()
 
-    def __draw_one_border(self, rect: pygame.Rect, border_width: int, border_gap: int):
+    def draw_one_border(self, rect: pygame.Rect, border_width: int, border_gap: int):
         pygame.draw.rect(self.surface, BORDER_COLOR, rect)
         rect = pygame.Rect(rect.x + border_width, rect.y + border_width,
                            rect.width - 2 * border_width, rect.height - 2 * border_width)
         pygame.draw.rect(self.surface, BACKGROUND_COLOR, rect)
-        return pygame.Rect(rect.x + border_gap, rect.y + border_gap,
+        self.rect_without_border = pygame.Rect(rect.x + border_gap, rect.y + border_gap,
                            rect.width - 2 * border_gap, rect.height - 2 * border_gap)
 
     def __draw_borders(self):
-        available_rect = self.surface.get_rect()
-        available_rect = self.__draw_one_border(available_rect, FIRST_BORDER_WIDTH, BORDER_GAP)
-        available_rect = self.__draw_one_border(available_rect, SECOND_BORDER_WIDTH, 0)
-        pygame.draw.rect(self.surface, self.background_color, available_rect)
-        return available_rect
+        self.draw_one_border(self.surface.get_rect(), FIRST_BORDER_WIDTH, BORDER_GAP)
+        self.draw_one_border(self.rect_without_border, SECOND_BORDER_WIDTH, 0)
+        pygame.draw.rect(self.surface, self.background_color, self.rect_without_border)
 
     def get_height(self):
         return self.surface.get_height()
@@ -52,3 +48,6 @@ class Widget:
 
     def get_screen_bottom(self):
         return self.get_screen_top() + self.get_height()
+
+    def handle_event(self, event : pygame.event.Event):
+        pass
