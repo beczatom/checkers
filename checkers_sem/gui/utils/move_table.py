87e84f2e1b43bdd849353e314486e07b784a7888
beczatom <beczatom@fit.cvut.cz>
2025-05-02 21:12:02 +0200

    Move table redraws only necessary moves

--- checkers_sem/gui/utils/move_table.py
+++ checkers_sem/gui/utils/move_table.py
@@ -12,52 +12,62 @@ class MoveTable(Widget):
         self.draw_border()
 
         self.font = pygame.font.Font(DEFAULT_FONT, 4 * DEFAULT_FONT_SIZE // 5)
-        self.row_height = DEFAULT_FONT_SIZE
-        self.row_padding_top = DEFAULT_FONT_SIZE // 2
+        self.row_height = 3 * DEFAULT_FONT_SIZE // 2
         self.move_texts = []
         self.offset_top = 0
-        self.printing_rect_height = 0
+        self.max_offset_top = 0
+
+        self.printed_indexes = None
 
     def set_move_texts(self, moves : list[Move]):
         self.move_texts = [str(i + 1) + '. ' + move_to_display_string(move) for i, move in enumerate(moves)]
+        self.max_offset_top = (len(self.move_texts) + 1) * self.row_height - self.rect_without_border.height
+        self.max_offset_top = max(self.max_offset_top, 0)
+        if self.offset_top > self.max_offset_top:
+            self.offset_top = self.max_offset_top
         self.draw()
 
     def handle_event(self, event : pygame.event.Event):
         if event.type == pygame.MOUSEBUTTONDOWN and self.screen_rect.collidepoint(event.pos):
             if event.button == 4: # scroll up
-                self.offset_top -= 10
+                self.offset_top -= self.row_height
 
-                if self.offset_top < 0:
+                if self.offset_top <= 0:
                     self.offset_top = 0
 
 
             if event.button == 5: # scroll down
-                self.offset_top += 10
-                if self.offset_top > self.printing_rect_height - self.rect_without_border.height:
-                    self.offset_top = self.printing_rect_height - self.rect_without_border.height
+                self.offset_top += self.row_height
+                if self.offset_top > self.max_offset_top:
+                    self.offset_top = self.max_offset_top
 
             self.draw()
 
+    def clear_indexes(self):
+        self.printed_indexes = None
+
     def draw(self):
+
+
+        start_idx = self.offset_top // self.row_height
+        end_idx = min(start_idx + self.rect_without_border.height // self.row_height, len(self.move_texts))
+
+        if self.printed_indexes == (start_idx, end_idx):
+            return
+
+        self.printed_indexes = (start_idx, end_idx)
+
         self.draw_border()
         self.surface.fill(BACKGROUND_COLOR, self.rect_without_border)
 
-        long_surface = pygame.Surface((self.rect_without_border.width, 10000))
-        long_surface.fill(BACKGROUND_COLOR)
 
-        top, left = self.rect_without_border.topleft[0], 0
+
+        top, left = self.rect_without_border.topleft
         size_x = self.rect_without_border.width
 
-        for move_text in self.move_texts:
-            top += self.row_padding_top
+        for move_text in self.move_texts[start_idx : end_idx]:
             text_rect = pygame.Rect(left, top, size_x, self.row_height)
-            Text(long_surface.subsurface(text_rect),
+            Text(self.surface.subsurface(text_rect),
                  tuple_sum(self.screen_left_top, (left, top)),
                  move_text, font_size = 4 * DEFAULT_FONT_SIZE // 5).draw()
             top += self.row_height
-
-        self.printing_rect_height = top
-
-        to_show_rect = self.rect_without_border.copy()
-        to_show_rect.top += self.offset_top
-        self.surface.blit(long_surface, self.rect_without_border, area=to_show_rect)
