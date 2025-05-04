d23753769beff3998dc461df392b5c731016a2ec
beczatom <beczatom@fit.cvut.cz>
2025-05-05 00:47:08 +0200

    Code style test

---
+++ tests/test_codestyle.py
@@ -0,0 +1,27 @@
+import inspect
+import pytest
+from pylint.lint import Run
+from pylint.reporters import CollectingReporter
+
+from checkers_sem.game import board, game, move
+
+# I was inspired by BI-PYT homework testing
+@pytest.mark.parametrize('file', [move, board, game])
+def test_codestyle(file):
+    """ Tests codestyle for given files. """
+    src_file = inspect.getfile(file)
+    rep = CollectingReporter()
+    # disabled warnings:
+    # 0301 line too long
+    # 0103 variables name (does not like shorter than 2 chars)
+    # 0719 too general exception (needed for invalid trees without defining own exception class)
+    res = Run(['--disable=C0301,C0103', '-sn', src_file], reporter=rep, exit=False)
+
+    score = res.linter.stats.global_note
+
+    for m in res.linter.reporter.messages:
+        print(f'{m.msg_id} ({m.symbol}) line {m.line}: {m.msg}')
+
+    print(f'pylint score = {score} ({score * 10:.2f}%)')
+
+    assert score >= 10
