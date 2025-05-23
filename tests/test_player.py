0aab56f0e630beed3adf2ca565d753f3ac33f6b2
beczatom <beczatom@fit.cvut.cz>
2025-05-24 00:13:27 +0200

    Player tests

---
+++ tests/test_player.py
@@ -0,0 +1,12 @@
+
+from checkers_sem.genetic.genetic_player import GeneticPlayer
+from checkers_sem.gui.utils.chessboard import ChessBoard
+from checkers_sem.constants import AI_COEFS, MAX_TRAIN_DEPTH
+from checkers_sem.game.move import Move
+from checkers_sem.game.game import Game
+
+def test_move_ai_player():
+    pass
+
+def test_move_human_player():
+    pass
