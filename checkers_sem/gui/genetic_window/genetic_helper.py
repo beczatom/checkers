241dadf6df8700d74d63b4b46b9ae62b231a83b3
beczatom <beczatom@fit.cvut.cz>
2025-05-26 03:19:13 +0200

    Genetic helper in simpler parts

---
+++ checkers_sem/gui/genetic_window/genetic_helper.py
@@ -0,0 +1,79 @@
+"""
+This module helps with performing genetic train
+"""
+
+from threading import Thread
+import queue
+
+from checkers_sem.genetic.genetic import Genetic
+from checkers_sem.helper import do_one_generation_thread, get_genetic_completion, choose_best_thread
+from checkers_sem.state import state
+
+
+class GeneticHelper:
+    """
+    This class helps GeneticWindow perform genetic train
+    """
+
+    def __init__(self):
+        """
+        Initialise the genetic helper
+        """
+        self.genetic = Genetic()
+        self.current_generation = 0
+        self.genetic_thread = Thread(target=do_one_generation_thread, args=(self.genetic,))
+        self.genetic_thread.start()
+        self.best_queue = queue.Queue()
+        self.best_player = None
+        self.average_coefs = self.genetic.get_average_coefs()
+
+    def get_coefs(self) -> list[float]:
+        """
+        Gets average coefficients if training or the best if finished picking the best
+        Returns
+        -------
+        coefs : list[float]
+            coefficients of the current generation or the best player
+        """
+        if self.best_player is None:
+            return [round(value, 3) for value in self.average_coefs]
+        return [round(value, 3) for value in self.best_player.coefs]
+
+    def get_completed_pct(self) -> float:
+        """
+        Gets how much of the work was done.
+        Returns
+        -------
+        pct : float
+            percentage of the work done
+        """
+        return 1 if self.best_player else get_genetic_completion(self.current_generation)
+
+    def check_thread(self) -> None:
+        """
+        Checks if genetic thread is running,
+        if not then visualizes the partial results and,
+        if necessary, starts a new one.
+        """
+
+        # we are done or we are working -> don't disturb us
+        if self.best_player or self.genetic_thread.is_alive():
+            return
+
+        self.genetic_thread.join()
+
+        # we were choosing best
+        if self.current_generation == state.GENERATIONS:
+            self.best_player = self.best_queue.get()
+            return
+
+        self.average_coefs = self.genetic.get_average_coefs()
+
+        self.current_generation += 1
+        # decide whether continue training or choose best
+        if self.current_generation < state.GENERATIONS:
+            self.genetic_thread = Thread(target=do_one_generation_thread, args=(self.genetic,))
+        else:
+            self.genetic_thread = Thread(target=choose_best_thread, args=(self.genetic, self.best_queue))
+
+        self.genetic_thread.start()
--- checkers_sem/gui/genetic_window/genetic_window.py
+++ checkers_sem/gui/genetic_window/genetic_window.py
@@ -1,92 +1,60 @@
-from checkers_sem.gui.utils.window import Window
+"""
+This module does the genetic training and shows the partial results
+"""
 
+import pygame
+
+from checkers_sem.gui.utils.window import Window
 from checkers_sem.gui.utils.text import Text
 from checkers_sem.gui.utils.progress_bar import ProgressBar
 from checkers_sem.gui.utils.pos import Pos
 from checkers_sem.state import state
-from threading import Thread
-from checkers_sem.helper import do_one_generation_thread, get_coefs_header_text, get_genetic_completion, choose_best_thread
-from checkers_sem.genetic.genetic import Genetic
-import queue
-import pygame
+from checkers_sem.helper import get_coefs_header_text
+from checkers_sem.gui.constants import BACKGROUND_COLOR, BEST_GENETIC_COEFICIENTS_TEXT, \
+    AVERAGE_GENETIC_COEFICIENTS_TEXT, DEFAULT_FONT_SIZE, STAT_TEXTS, GENETIC_SETTINGS_TEXT, SLIDER_PROPERTIES
+from checkers_sem.gui.genetic_window.genetic_helper import GeneticHelper
 
-from checkers_sem.gui.constants import BACKGROUND_COLOR, BEST_GENETIC_COEFICIENTS_TEXT, AVERAGE_GENETIC_COEFICIENTS_TEXT, DEFAULT_FONT_SIZE, STAT_TEXTS, GENETIC_SETTINGS_TEXT, SLIDER_PROPERTIES
 
 class GeneticWindow(Window):
-    def __init__(self, surface : pygame.surface):
+    """
+    This class shows the user how the genetic training is going
+    """
+
+    def __init__(self, surface: pygame.surface):
         super().__init__(surface)
         self.surface.fill(BACKGROUND_COLOR)
         pygame.display.flip()
-        self.genetic = Genetic()
-        self.average_coefs = self.genetic.get_average_coefs()
-        self.current_generation = 0
-        self.completed = False
-        self.genetic_thread = Thread(target=do_one_generation_thread, args=(self.genetic,))
-        self.genetic_thread.start()
-        self.setting_header, self.setting_val_headers, self.setting_values = self.init_setting_options()
-        self.coefs_header, self.average_coefs_headers, self.average_coefs_text  = self.init_average_texts()
-        self.bar = self.init_bar()
-        self.best_queue = queue.Queue()
-        self.best_player = None
 
-    def update_coefs(self):
-        if self.best_player is None:
-            self.coefs_header.set_string(get_coefs_header_text(self.current_generation + 1))
-            self.coefs_header.draw()
-            for i, text in enumerate(self.average_coefs_text):
-                text.set_string(round(self.average_coefs[i], 3))
-                text.draw()
-            return
-
-        self.coefs_header.set_string(BEST_GENETIC_COEFICIENTS_TEXT)
-        self.coefs_header.draw()
-        for i, text in enumerate(self.average_coefs_text):
-            text.set_string(round(self.best_player.coefs[i], 3))
-            text.draw()
-
-    def update_bar(self):
-        completed_pct = 1 if self.completed else get_genetic_completion(self.current_generation)
-        self.bar.set_value(completed_pct)
-        self.bar.draw()
-
-    def check_thread(self):
-        if self.completed: return
-        if self.genetic_thread.is_alive(): return
-
-        self.genetic_thread.join()
-
-        # we were choosing best
-        if self.current_generation == state.GENERATIONS:
-            self.best_player = self.best_queue.get()
-            self.completed = True
-
-        self.average_coefs = self.genetic.get_average_coefs()
-        self.update_coefs()
-
-        if self.best_player is not None:
-            self.update_bar()
-            return
-
-        self.current_generation += 1
-        if self.current_generation < state.GENERATIONS:
-            self.genetic_thread = Thread(target=do_one_generation_thread, args=(self.genetic,))
-        else:
-            self.genetic_thread = Thread(target=choose_best_thread, args=(self.genetic, self.best_queue))
-
-        self.genetic_thread.start()
-        self.update_bar()
+        self.genetic_helper = GeneticHelper()
+        self.settings = self.init_setting_options()
+        self.coefs = self.init_average_texts()
+        self.progress_bar = self.init_bar()
 
     def init_bar(self) -> ProgressBar:
-        bar = ProgressBar(self.surface,
-                  Pos((0.7, 0.15), (0.15, 0.15, 0.7, 0.15)),
-                  value = 0)
-        return bar
+        """
+        Inits progress bar.
+        Returns
+        -------
+        progress_bar : ProgressBar
+            initialized progress bar
+        """
+        progress_bar = ProgressBar(self.surface,
+                                   Pos((0.7, 0.15), (0.15, 0.15, 0.7, 0.15)),
+                                   value=0)
+        return progress_bar
 
     def init_average_texts(self) -> tuple[Text, list[Text], list[Text]]:
+        """
+        Initializes average coefficients texts.
+        Returns
+        -------
+        header, text_val_header, text_val : tuple[Text, list[Text], list[Text]]
+            header of the block, header of coefficient and the coefficient itself
+        """
         header_text = AVERAGE_GENETIC_COEFICIENTS_TEXT + ' pri inicializácii'
         coefs_header = Text(self.surface,
                             Pos((0.45, 0.1), (0.35, 0.05, 0.55, 0.5)),
-                            text = header_text)
+                            text=header_text)
 
         coefs_val_headers = []
         coefs_vals = []
@@ -95,18 +63,25 @@ class GeneticWindow(Window):
 
             coefs_val_headers.append(Text(self.surface,
                                           Pos((0.275, 0.075), (0.475 + i * 0.075, 0.05, 0.45 - i * 0.075, 0.55)),
-                                          text = stat_name, font_size=font_size))
+                                          text=stat_name, font_size=font_size))
 
             coefs_vals.append(Text(self.surface,
                                    Pos((0.125, 0.075), (0.475 + i * 0.075, 0.05, 0.45 - i * 0.075, 0.8)),
-                                   text = round(self.average_coefs[i], 3), font_size=font_size))
+                                   text=self.genetic_helper.get_coefs()[i], font_size=font_size))
 
         return coefs_header, coefs_val_headers, coefs_vals
 
     def init_setting_options(self) -> tuple[Text, list[Text], list[Text]]:
+        """
+        Initializes setting texts.
+        Returns
+        -------
+        header, text_val_header, text_val : tuple[Text, list[Text], list[Text]]
+            header of the block, header of setting and the setting itself
+        """
         header = Text(self.surface,
                       Pos((0.45, 0.1), (0.35, 0.5, 0.55, 0.05)),
-                      text = GENETIC_SETTINGS_TEXT)
+                      text=GENETIC_SETTINGS_TEXT)
         setting_val_headers = []
         setting_val_texts = []
 
@@ -118,25 +93,49 @@ class GeneticWindow(Window):
 
             setting_val_headers.append(Text(self.surface,
                                             Pos((0.275, 0.075), (0.475 + i * 0.075, 0.675, 0.45 - i * 0.075, 0.05)),
-                                            text = text, font_size = font_size))
+                                            text=text, font_size=font_size))
 
             setting_val_texts.append(Text(self.surface,
                                           Pos((0.125, 0.075), (0.475 + i * 0.075, 0.55, 0.45 - i * 0.075, 0.325)),
-                                          text = setting_val, font_size = font_size))
+                                          text=setting_val, font_size=font_size))
 
         return header, setting_val_headers, setting_val_texts
 
-    def handle_event(self, event : pygame.event.Event):
-        super().handle_event(event)
+    def update_coefs(self) -> None:
+        """
+        Update average or final coefficients of training.
+        """
+        if self.genetic_helper.best_player is None:
+            self.coefs[0].set_text(get_coefs_header_text(self.genetic_helper.current_generation))
+        else:
+            self.coefs[0].set_text(BEST_GENETIC_COEFICIENTS_TEXT)
 
-    def refresh(self):
-        self.check_thread()
-        self.setting_header.draw()
-        for i in range(len(self.setting_val_headers)):
-            self.setting_val_headers[i].draw()
-            self.setting_values[i].draw()
+        self.coefs[0].draw()
+        for i, text in enumerate(self.coefs[2]):
+            text.set_text(self.genetic_helper.get_coefs()[i])
+            text.draw()
 
-        self.coefs_header.draw()
-        for i in range(len(self.average_coefs)):
-            self.average_coefs_headers[i].draw()
-            self.average_coefs_text[i].draw()
+    def update_bar(self) -> None:
+        """
+        Updates progress bar.
+        """
+        completed_pct = self.genetic_helper.get_completed_pct()
+        self.progress_bar.set_value(completed_pct)
+        self.progress_bar.draw()
+
+    def refresh(self) -> None:
+        """
+        Redraws the window
+        """
+        self.genetic_helper.check_thread()
+        self.update_coefs()
+        self.update_bar()
+        self.settings[0].draw()
+        for i, header in enumerate(self.settings[1]):
+            header.draw()
+            self.settings[2][i].draw()
+
+        self.coefs[0].draw()
+        for i in range(len(self.coefs[1])):
+            self.coefs[1][i].draw()
+            self.coefs[2][i].draw()
