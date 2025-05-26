4676276a63256f628169763adedfa90953a8e2de
beczatom <beczatom@fit.cvut.cz>
2025-05-26 14:40:58 +0200

    Game window codestyle

--- checkers_sem/gui/game_window/game_display.py
+++ checkers_sem/gui/game_window/game_display.py
@@ -1,28 +1,27 @@
+"""
+This submodule helps GameWindow with taking care of chessboard, timers and evaluation.
+"""
 
-import copy
 from threading import Thread
-from typing import Callable
-from abc import abstractmethod
 
 import pygame
 
 from checkers_sem.gui.utils.widget import Widget
-from checkers_sem.game.game import Game, BitBoard
-from checkers_sem.player.player import Player, AIPlayer
+from checkers_sem.game.game import Game
 from checkers_sem.gui.utils.chessboard import ChessBoard
 from checkers_sem.gui.utils.timer import Timer
 from checkers_sem.gui.utils.text import Text
-from checkers_sem.gui.utils.window import Window
-from checkers_sem.gui.utils.move_table import MoveTable
-from checkers_sem.gui.utils.button import ImageButton
-from checkers_sem.gui.utils.result import Result
-from checkers_sem.gui.utils.checkbox import CheckBox
 from checkers_sem.state import state
 from checkers_sem.gui.utils.pos import Pos
-from checkers_sem.gui.constants import SHOW_BEST_MOVES_TEXT, LEFT_ARROW_IMAGE, RIGHT_ARROW_IMAGE, BACKGROUND_COLOR, RESTART_ARROW_IMAGE
 from checkers_sem.game.constants import Color, GameEnd
+from checkers_sem.gui.game_window.eval_helper import EvalHelper
+
 
 class GameDisplay(Widget):
+    """
+    This class helps GameWindow with taking care of chessboard, timers and evaluation.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args)
 
@@ -43,10 +42,13 @@ class GameDisplay(Widget):
 
         self.active_thread = None
 
-
     def make_move(self) -> bool:
         """
         Takes care of move making on displayed chessboard.
+        Returns
+        -------
+        move_made : bool
+            true if move was made
         """
         if self.res != (None, None):
             return False
@@ -86,10 +88,13 @@ class GameDisplay(Widget):
             self.timers[Color.WHITE].time_stop()
             self.timers[Color.BLACK].time_stop()
 
-
     def move_ai(self) -> bool:
         """
         Defines move changes for AI player
+        Returns
+        -------
+        move_made : bool
+            true if move was made
         """
         if self.active_thread is None and self.res == (None, None):
             self.active_thread = Thread(target=self.make_move)
@@ -106,7 +111,6 @@ class GameDisplay(Widget):
             self.active_thread = None
             return True
         return False
-
 
     def start_times(self) -> None:
         """
@@ -129,13 +133,13 @@ class GameDisplay(Widget):
         timers : dict[bool, Timer]
             timers
         """
-        timers = dict()
+        timers = {}
         timers[Color.BLACK] = Timer(self.surface,
-                            Pos((0.2, 0.075), (0, 0, 0.858, 0.8)),
+                                    Pos((0.25, 0.08), (0, 0, 0.92, 0.75)),
                                     self.left_top)
 
         timers[Color.WHITE] = Timer(self.surface,
-                            Pos((0.2, 0.075), (0.858, 0, 0, 0.8)),
+                                    Pos((0.25, 0.08), (0.92, 0, 0, 0.75)),
                                     self.left_top)
         return timers
 
@@ -148,9 +152,9 @@ class GameDisplay(Widget):
             the chessboard
         """
         return ChessBoard(self.surface,
-                          Pos((1, 0.8), (0.1, 0, 0.1, 0), center=True),
+                          Pos((1, 0.78), (0.11, 0, 0.11, 0), center=True),
                           self.screen_left_top,
-                          game = Game())
+                          game=Game())
 
     def init_eval_texts(self) -> dict[bool, Text]:
         """
@@ -163,12 +167,12 @@ class GameDisplay(Widget):
         eval_texts = {}
 
         eval_texts[Color.BLACK] = Text(self.surface,
-                                       Pos((0.2, 0.075), (0, 0, 0.858, 0), center=True),
+                                       Pos((0.25, 0.08), (0, 0, 0.91, 0), center=True),
                                        self.left_top,
                                        text='')
 
         eval_texts[Color.WHITE] = Text(self.surface,
-                                       Pos((0.2, 0.075), (0.8, 0, 0, 0), center=True),
+                                       Pos((0.25, 0.08), (0.91, 0, 0, 0), center=True),
                                        self.left_top,
                                        text='')
 
@@ -183,69 +187,15 @@ class GameDisplay(Widget):
         elif self.timers[Color.BLACK].time_is_over():
             self.res = (1, GameEnd.NO_TIME)
 
-    def handle_event(self, event: pygame.event.Event):
-        pass
-
-    def draw(self) -> None:
-        pass
-
-class EvalHelper:
-    def __init__(self, chessboard : ChessBoard, texts : dict[bool, Text]):
-        self.chessboard = chessboard
-        self.texts = texts
-        self.eval_values = {Color.BLACK: 0.0, Color.WHITE: 0.0}
-        self.evaluation_start_hash = hash(None)
-        self.best_move = None
-        self.active_thread = None
-
-    def get_eval(self) -> tuple[dict[bool, float], tuple[BitBoard, BitBoard]]:
-        # if we are working and have result
-        if self.active_thread is not None and not self.active_thread.is_alive():
-            self.active_thread.join()
-            if self.evaluation_start_hash != hash(self.chessboard.game):
-                self.chessboard.reset_best_move()
-                self.best_move = None
-            self.active_thread = None
-
-        # if something changed and we are not working on it
-        if self.active_thread is None and self.evaluation_start_hash != hash(self.chessboard.game):
-            self.evaluation_start_hash = hash(self.chessboard.game)
-            self.active_thread = Thread(target=self.evaluating_thread, args=(copy.deepcopy(self.chessboard.game),))
-            self.active_thread.start()
-
-        return self.eval_values, self.best_move
-
-    def set_eval(self, evaluation : float, color : bool):
-        self.eval_values[color] = evaluation
-        self.evaluation_start_hash = hash(self.chessboard.game)
-        # self.best_move = None
-
-    def update_eval(self):
-        self.get_eval()
-        for color in [Color.WHITE, Color.BLACK]:
-            if self.eval_values[color] is not None:
-                self.texts[color].set_text(f'{self.eval_values[color]:.3f}')
-                self.texts[color].draw()
-
-    def update_best_move(self):
-        if self.chessboard.best_move is None and self.evaluation_start_hash == hash(self.chessboard.game) and \
-                self.active_thread is None:
-            self.get_eval()
-            self.chessboard.set_best_move(self.best_move)
-            self.chessboard.draw()
-
-    def evaluating_thread(self, game: Game) -> None:
+    def handle_event(self, event: pygame.event.Event) -> None:
         """
-        Function for thread to evaluate the game.
+        Does nothing.
         Parameters
         ----------
-        game : Game
-            game to evaluate
+        event : pygame.event.Event
         """
-        player = AIPlayer(state.COEFS_BLACK)
-        player.set_game(game)
-        _, _, best = player.move(state.DEPTH_BLACK)
-        game.pop()
-        self.eval_values[Color.BLACK] = best[1]
-        self.best_move = (best[0].from_mask, best[0].to_mask)
 
+    def draw(self) -> None:
+        """
+        Does nothing.
+        """
--- checkers_sem/gui/game_window/game_window.py
+++ checkers_sem/gui/game_window/game_window.py
@@ -2,27 +2,21 @@
 This module defines main game window.
 """
 
-import copy
-from threading import Thread
 from typing import Callable
 from abc import abstractmethod
 
 import pygame
 
-from checkers_sem.game.game import Game
-from checkers_sem.player.player import Player, AIPlayer
-from checkers_sem.gui.utils.chessboard import ChessBoard
-from checkers_sem.gui.utils.timer import Timer
+from checkers_sem.player.player import Player
 from checkers_sem.gui.utils.text import Text
 from checkers_sem.gui.utils.window import Window
 from checkers_sem.gui.utils.move_table import MoveTable
 from checkers_sem.gui.utils.button import ImageButton
 from checkers_sem.gui.utils.result import Result
 from checkers_sem.gui.utils.checkbox import CheckBox
-from checkers_sem.state import state
 from checkers_sem.gui.utils.pos import Pos
 from checkers_sem.gui.constants import SHOW_BEST_MOVES_TEXT, LEFT_ARROW_IMAGE, RIGHT_ARROW_IMAGE, BACKGROUND_COLOR, RESTART_ARROW_IMAGE
-from checkers_sem.game.constants import Color, GameEnd
+from checkers_sem.game.constants import Color
 from checkers_sem.gui.game_window.game_display import GameDisplay
 
 class GameWindow(Window):
@@ -32,18 +26,7 @@ class GameWindow(Window):
     def __init__(self, surface : pygame.surface, players : tuple[Player, Player]):
         super().__init__(surface)
 
-        # self.chessboard = self.init_chessboard()
-        # self.chessboard.draw()
-        #
         self.players = players
-        # self.players[0].set_chessboard(self.chessboard)
-        # self.players[1].set_chessboard(self.chessboard)
-        #
-        # self.timers = self.init_timers()
-        #
-        # self.res = None
-        # self.game_end_type = None
-
         self.game_display = self.init_game_display()
 
         self.move_table = self.init_move_table()
@@ -55,13 +38,10 @@ class GameWindow(Window):
         self.res_window = None
         self.res_window_showed = False
 
-        # self.eval_values = {Color.BLACK: None, Color.WHITE: None}
-        # self.eval_texts = self.init_eval_texts()
-        # self.evaluation_start_hash = hash(None)
-        # self.evaluating = False
-        # self.best_move = None
-
     def init_game_display(self) -> GameDisplay:
+        """
+        Initializes game display.
+        """
         game_display = GameDisplay(self.surface,
                     Pos((0.5, 0.916), (0.042, 0.45, 0.042, 0.05)),
                     players = self.players)
@@ -97,35 +77,6 @@ class GameWindow(Window):
                             on_check=self.checkbox_on_check,
                             on_uncheck=self.checkbox_on_uncheck)
         return text, checkbox
-
-    # def init_eval_texts(self) -> dict[bool, Text]:
-    #     """
-    #     Initializes evaluation texts dictionary for both of players.
-    #     Returns
-    #     -------
-    #     eval_dict : dict[bool, Text]
-    #         white and black eval_text
-    #     """
-    #     eval_texts = {}
-    #
-    #     eval_texts[Color.BLACK] = Text(self.surface,
-    #                                    Pos((0.1, 0.075), (0.067, 0.65, 0.858, 0.25), center=True),
-    #                                    text='')
-    #
-    #     eval_texts[Color.WHITE] = Text(self.surface,
-    #                                    Pos((0.1, 0.075), (0.858, 0.65, 0.067, 0.25), center=True),
-    #                                    text='')
-    #
-    #     return eval_texts
-
-    # def update_eval_texts(self) -> None:
-    #     """
-    #     Updates evaluation texts.
-    #     """
-    #     for color in [Color.BLACK, Color.WHITE]:
-    #         if self.eval_values[color] is not None:
-    #             self.eval_texts[color].set_text(f'{self.eval_values[color]:.3f}')
-    #         self.eval_texts[color].draw()
 
     def get_button_control_function(self, i : int) -> Callable[[], None]:
         """
@@ -200,46 +151,6 @@ class GameWindow(Window):
         move_table.draw()
         return move_table
 
-    # def init_timers(self) -> tuple[Timer, Timer]:
-    #     """
-    #     Initializes timers.
-    #     Returns
-    #     -------
-    #     white_timer, black_timer : tuple[Timer, Timer]
-    #         timers
-    #     """
-    #     black_timer = Timer(self.surface,
-    #                         Pos((0.125, 0.075), (0.067, 0.45, 0.858, 0.425)))
-    #
-    #     white_timer = Timer(self.surface,
-    #                         Pos((0.125, 0.075), (0.858, 0.45, 0.067, 0.425)))
-    #     return white_timer, black_timer
-    #
-    # def init_chessboard(self) -> ChessBoard:
-    #     """
-    #     Initializes chessboard.
-    #     Returns
-    #     -------
-    #     chessboard : ChessBoard
-    #         the chessboard
-    #     """
-    #     return ChessBoard(self.surface,
-    #                       Pos((0.5, 0.715), (0.142, 0.45, 0.142, 0.05), center=True),
-    #                       game = Game())
-    #
-    # def start_times(self) -> None:
-    #     """
-    #     Starts that timer, which player is on turn
-    #     """
-    #     if self.res is not None:
-    #         return
-    #     if self.turn == Color.WHITE:
-    #         self.timers[0].time_start()
-    #         self.timers[1].time_stop()
-    #     else:
-    #         self.timers[0].time_stop()
-    #         self.timers[1].time_start()
-    #
     def result_onclick(self) -> None:
         """
         Defines onclick for result widget
@@ -266,74 +177,15 @@ class GameWindow(Window):
         return Result(self.surface,
                       Pos((0.5, 0.5), (0, 0, 0, 0), center=True),
                       res = self.game_display.res, onclick = self.result_onclick)
-    #
-    # def make_move(self) -> None:
-    #     """
-    #     Takes care of move making on displayed chessboard.
-    #     """
-    #     if self.res is not None:
-    #         return
-    #
-    #     if not self.timers[0].time_going and not self.timers[1].time_going:
-    #         self.start_times()
-    #
-    #     was_performed = False
-    #     if self.turn == Color.WHITE:
-    #         no_moves, was_performed, best = self.players[0].move(state.DEPTH_WHITE)
-    #     else:
-    #         no_moves, was_performed, best = self.players[1].move(state.DEPTH_BLACK)
-    #
-    #     if no_moves:
-    #         self.res = -1 if self.turn == Color.WHITE else 1
-    #         self.game_end_type = GameEnd.NO_MOVES
-    #         self.res_window = self.init_result_window()
-    #         return
-    #
-    #     if was_performed:
-    #         if best is not None:
-    #             self.eval_values[self.turn] = best[1]
-    #         self.turn = self.chessboard.game.board.turn
-    #         self.move_table.set_move_texts(self.chessboard.game.get_move_history())
-    #         self.start_times()
-    #
-    #     self.res = self.chessboard.game.get_result()
-    #     if self.res is not None:
-    #         self.game_end_type = self.chessboard.game.get_end_type()
-    #
+
     def check_game_end(self) -> None:
         """
         Checks if game is over and does corresponding actions.
         """
+        self.game_display.check_game_end()
         if self.res_window_showed or self.game_display.res == (None, None):
             return
-        # if self.res is None:
-        #     self.res = self.chessboard.game.get_result()
-        # if self.res is not None:
-        # self.chessboard.reset_best_move()
-        # self.timers[0].time_stop()
-        # self.timers[1].time_stop()
-        # if self.game_end_type is None:
-        #     self.game_end_type = self.chessboard.game.get_end_type()
         self.res_window = self.init_result_window()
-    #
-    #
-    # def move_ai(self) -> None:
-    #     """
-    #     Defines move changes for AI player
-    #     """
-    #     if self.active_thread is None and self.res is None:
-    #         self.active_thread = Thread(target=self.make_move)
-    #         self.active_thread.start()
-    #
-    #     if self.active_thread is not None and not self.active_thread.is_alive():
-    #         self.active_thread.join()
-    #         self.turn = self.chessboard.game.board.turn
-    #         if self.game_end_type == GameEnd.NO_TIME:
-    #             self.chessboard.game.pop()
-    #         self.active_thread = None
-    #
-    #         if self.res is None:
-    #             self.chessboard.draw()
 
     def handle_event(self, event : pygame.event.Event) -> None:
         """
@@ -346,55 +198,6 @@ class GameWindow(Window):
         super().handle_event(event)
         if self.res_window is not None:
             self.res_window.handle_event(event)
-
-    # def time_over_check(self) -> None:
-    #     """
-    #     Checks if time is over.
-    #     """
-    #     if self.timers[0].time_is_over():
-    #         self.res = -1
-    #         self.game_end_type = GameEnd.NO_TIME
-    #     elif self.timers[1].time_is_over():
-    #         self.res = 1
-    #         self.game_end_type = GameEnd.NO_TIME
-    #
-    # def evaluating_thread(self, game: Game) -> None:
-    #     """
-    #     Function for thread to evaluate the game.
-    #     Parameters
-    #     ----------
-    #     game : Game
-    #         game to evaluate
-    #     """
-    #     player = AIPlayer(state.COEFS_BLACK)
-    #     player.set_game(game)
-    #     _, _, best = player.move(state.DEPTH_BLACK)
-    #     game.pop()
-    #     self.eval_values[Color.BLACK] = best[1]
-    #     if best[0] is not None:
-    #         self.best_move = (best[0].from_mask, best[0].to_mask)
-    #
-    # def evaluating_thread_check(self) -> None:
-    #     """
-    #     Checks if thread finished evaluating.
-    #     Or if it is necessary to start it.
-    #     """
-    #     if self.res is not None:
-    #         return
-    #     if self.active_thread is None and self.evaluation_start_hash != hash(self.chessboard.game.board) and not self.evaluating:
-    #         self.evaluation_start_hash = hash(self.chessboard.game.board)
-    #         self.evaluating = True
-    #         self.active_thread = Thread(target=self.evaluating_thread, args=(copy.deepcopy(self.chessboard.game),))
-    #         self.active_thread.start()
-    #
-    #     if self.active_thread is not None and not self.active_thread.is_alive() and self.evaluating:
-    #         self.active_thread.join()
-    #         self.evaluating = False
-    #         if self.evaluation_start_hash == hash(self.chessboard.game.board):
-    #             self.chessboard.set_best_move(self.best_move)
-    #         if self.res is None:
-    #             self.chessboard.draw()
-    #         self.active_thread = None
 
     @abstractmethod
     def refresh(self) -> None:
