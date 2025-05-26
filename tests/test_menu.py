8e89ece48862fdaa1b928ed1cc8cac274fac9aec
beczatom <beczatom@fit.cvut.cz>
2025-05-26 23:06:50 +0200

    Menu test

---
+++ tests/test_menu.py
@@ -0,0 +1,21 @@
+"""
+This module tests menu window
+"""
+
+import pygame
+
+from checkers_sem.gui.constants import BACKGROUND_COLOR, PLAY_BUTTON_TEXT, GENETIC_TEXT
+from checkers_sem.gui.menu import Menu
+
+def test_menu() -> None:
+    """
+    Menu testing
+    """
+    surface = pygame.Surface((800, 800))
+    surface.fill(BACKGROUND_COLOR)
+    menu = Menu(surface)
+    assert menu.screen == surface
+    assert len(menu.buttons) == 2
+    assert menu.buttons[0].text == PLAY_BUTTON_TEXT
+    assert menu.buttons[1].text == GENETIC_TEXT
+    assert menu.next_window is None
